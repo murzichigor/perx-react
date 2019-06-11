df4f0bf04fa0cf638dd0f79de5a1616856fcf0e3
Igor Murzich <murzichigordev@gmail.com>
2019-06-11 08:46:42 +0300

    Added condition to send Authorization header only when token exists.

--- src/api/apiSetup.js
+++ src/api/apiSetup.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 import { API_URL, TOKEN_KEY } from '../config';
 
+// Do not send credentials if there isn't a token
+let authorizationHeader = {};
+if (TOKEN_KEY) {
+  authorizationHeader = {
+    Authorization: `token ${TOKEN_KEY}`,
+  };
+}
+
 const api = axios.create({
   baseURL: API_URL,
   headers: {
-    Authorization: `token ${TOKEN_KEY}`,
+    ...authorizationHeader,
   },
 });
 
